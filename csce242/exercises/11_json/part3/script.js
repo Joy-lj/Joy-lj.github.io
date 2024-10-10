7c4b28fce2f5522a3af8bd5b4584ab36ea781b02
joy-lj <joyluya@gmail.com>
2024-10-10 13:10:31 -0400

    project

--- csce242/exercises/11_json/part3/script.js
+++ csce242/exercises/11_json/part3/script.js
@@ -1,1 +1,21 @@
 //"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+
+const getCocktails = async() => {
+    try {
+        return (await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")).json();
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+const showCocktails = async() => {
+    const cocktails = await getCocktails();
+
+    cocktails.forEach((cocktail)=> {
+        const section = document.createElement("section");
+        document.getElementById("cocktails").append(section);
+    });
+};
+
+//show all of the shoes when the page loads
+showCocktails();
