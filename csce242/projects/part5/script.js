c1911a91b757fe280374bf05a13dcb535420a6bf
joy-lj <joyluya@gmail.com>
2024-10-12 19:22:29 -0400

    json

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -21,21 +21,26 @@ const showBooks = async () => {
         document.getElementById("books-section").append(section);
 
         const a = document.createElement("a");
-        a.href = `https://joy-lj.github.io/csce242/projects/part5/bookdescrip.html`;  // Link to the specific page for each book
-        a.target = "_blank"; // Open link in a new tab
+        a.href = book.url;
+        a.classList.add("book-content");
         section.append(a);
 
-        const h3 = document.createElement("h3");
-        h3.innerHTML = book.name;
-        a.append(h3);
+        const name = document.createElement("h3");
+        name.innerHTML = book.name;
+        a.append(name);
 
         const author = document.createElement("p");
-        author.innerHTML = `Author: ${book.author}`;
-        section.append(author);
+        author.innerHTML = book.author;
+        a.append(author);
 
         const img = document.createElement("img");
         img.src = `https://joy-lj.github.io/csce242/projects/part5/images/bookcovers/${book.image}`;
-        section.append(img);
+        a.append(img);
+
+        const cite = document.createElement("p");
+        cite.classList.add("cite");
+        cite.innerHTML = book.cite;
+        section.append(cite);
 
         const availability = document.createElement("p");
         availability.innerHTML = `Availability: ${book.availability}`;
